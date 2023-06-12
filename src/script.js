93e1fedae8adec9a03750bf673e1cbdf5966533e
Roman Zubko <romanzubko93@gmail.com>
2023-06-12 15:21:59 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const canvas = document.getElementById('canvas');
+
+
+function createLine() {
+
+  //later transfer to object or add methods
+  return function() {
+
+  }
+}
+
+let line1 = createLine();
+line1();
+// let line2 = createLine();
